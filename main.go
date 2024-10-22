72a54f9fd6abbe4a9e936de246059c7b5b48416b
Abdur Rehman <abdur.rehman@10pearls.com>
2024-10-22 14:58:45 +0500

    add user name and user number of booked tickets

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application!")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	userName = "John"
+	userTickets = 3
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 }
