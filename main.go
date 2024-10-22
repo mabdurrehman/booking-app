aec80bab4c554d889c9ec8ee3d22d042a87ec230
Abdur Rehman <abdur.rehman@10pearls.com>
2024-10-22 14:41:07 +0500

    add welcome message and share tickets status message

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-	fmt.Println("hey there!")
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
+	fmt.Println("Welcome to", conferenceName, "booking application!")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets here to attend")
 }
